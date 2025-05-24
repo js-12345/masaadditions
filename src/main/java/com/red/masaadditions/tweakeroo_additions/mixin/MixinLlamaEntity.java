2f48262d8728469ae2e0fd17fe368f21f22b2748
js-12345 <schaeferjonathan@gmx.net>
2025-05-24 02:32:39 +0200

    fixing lama steering

--- src/main/java/com/red/masaadditions/tweakeroo_additions/mixin/MixinLlamaEntity.java
+++ src/main/java/com/red/masaadditions/tweakeroo_additions/mixin/MixinLlamaEntity.java
@@ -1,12 +1,14 @@
 package com.red.masaadditions.tweakeroo_additions.mixin;
 
 import com.red.masaadditions.tweakeroo_additions.config.FeatureToggleExtended;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.passive.AbstractDonkeyEntity;
 import net.minecraft.entity.passive.AbstractHorseEntity;
 import net.minecraft.entity.passive.AnimalEntity;
 import net.minecraft.entity.passive.LlamaEntity;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -22,7 +24,11 @@ public abstract class MixinLlamaEntity extends AnimalEntity {
     @Inject(method = "getControllingPassenger()Lnet/minecraft/entity/LivingEntity;", at = @At("HEAD"), cancellable = true)
     private void allowLamaSteering(CallbackInfoReturnable<LivingEntity> cir) {
         if (FeatureToggleExtended.TWEAK_LLAMA_STEERING.getBooleanValue()) {
-            cir.setReturnValue(super.getControllingPassenger());
+            Entity var2 = this.getFirstPassenger();
+            if (var2 instanceof PlayerEntity) {
+                PlayerEntity playerEntity = (PlayerEntity)var2;
+                cir.setReturnValue(playerEntity);
+            };
         }
     }
 }
