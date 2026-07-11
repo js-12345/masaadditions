cee49e59327151f1769b8f0a4adb210d2895f66f
js-12345 <schaeferjonathan@gmx.net>
2026-07-11 23:31:15 +0200

    Fixing crash in block entitiy renderers

--- src/main/java/com/red/masaadditions/tweakeroo_additions/mixin/MixinBeaconBlockEntityRenderer.java
+++ src/main/java/com/red/masaadditions/tweakeroo_additions/mixin/MixinBeaconBlockEntityRenderer.java
@@ -9,7 +9,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(BeaconBlockEntityRenderer.class)
 public abstract class MixinBeaconBlockEntityRenderer {
-    @Inject(method = "render(Lnet/minecraft/block/entity/BeaconBlockEntity;FLnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider;II)V", at = @At("HEAD"), cancellable = true)
+    @Inject(method = "render", at = @At("HEAD"), cancellable = true)
     private void render(CallbackInfo ci) {
         if (ConfigsExtended.Disable.DISABLE_BEACON_BEAM_RENDERING.getBooleanValue())
             ci.cancel();
